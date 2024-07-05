c7c571339ec244dc8c338cbd8a17a50095a41356
5456ju00 <160253571+5456ju00@users.noreply.github.com>
2024-07-05 11:19:54 -0300

    prontos

--- script_banco_biblioteca.sql
+++ script_banco_biblioteca.sql
@@ -3,19 +3,19 @@ create database biblioteca;
 use biblioteca;
 
 create table aluno(
-	pk int not null primary key auto_increment,
-    nome varchar(255)
+	pk int not null primary key auto_increment, primary key(pk),
+    nome varchar(255) not null
 );
 
 create table livro(
-	pk int not null primary key auto_increment,
-	nome varchar(255)
+	pk int not null primary key auto_increment, primary key(pk),
+	nome varchar(255) not null
 );
 
 create table emprestimo(
-	pk int not null primary key auto_increment,
-    data_emprestimo date,
-    data_devolucao date,
+	pk int not null primary key auto_increment, primary key(pk),
+    data_emprestimo date not null,
+    data_devolucao date not null,
 	aluno_fk int not null, foreign key (aluno_fk) references aluno (pk),
     livro_fk int not null, foreign key (livro_fk) references livro (pk)
 );
